e2b5b0a2478cfeb451d7992bbcd54f9c58c7c3dc
mikuu <biao.fu@thoughtworks.com>
2018-06-17 01:04:05 +0800

    Practice something

--- src/book/detail/views/detail.js
+++ src/book/detail/views/detail.js
@@ -19,7 +19,33 @@ const BookDetail = ({match}) => {
 
 class BookDetailComponent extends React.Component {
     constructor(props) {
+        console.log('FBI --> constructor');
+
         super(props);
+        this._getBook = this._getBook.bind(this);
+
+        this.state = {book:null};
+    }
+
+    componentDidMount() {
+        this._getBook();
+        console.log('FBI --> didMount: '+this.state.book);
+    }
+
+    _getBook() {
+        const apiUrl = 'http://localhost:3000/v2/book/'+this.props.match.params.bookId;
+        fetch(apiUrl).then((response) => {
+            if (response.status !== 200) {
+                throw new Error('Fail to get response with status ' + response.status);
+            }
+            response.json().then((responseJson) => {
+                this.setState({book: responseJson});
+            }).catch((error) => {
+                throw new Error('Failed to get response, reason: '+error.toString());
+            });
+        }).catch((error) => {
+            throw new Error('Failed to fetch request, reason: '+error.toString());
+        });
     }
 
     render() {
@@ -27,12 +53,38 @@ class BookDetailComponent extends React.Component {
             margin: '20px'
         };
 
+        if (this.state.book == null) {
+            return (
+                <div>
+                    <p>loading ...</p>
+                </div>
+            );
+        }
+
         return (
             <div style={styleDetail}>
-                {/*<Img src={props.image}/>*/}
-                {/*<h3>Title: {props.title}</h3>*/}
-                {/*<p>Price: {props.price}</p>*/}
-                {this.props.match.params.bookId}
+                <img src={this.state.book.image} width={'220px'} height={'300px'}/>
+                <div>
+                    <b>Title: </b>
+                    <span>{this.state.book.title}</span>
+                </div>
+                <div>
+                    <b>Price: </b>
+                    <span>{this.state.book.price}</span>
+                </div>
+                <div>
+                    <b>Publisher: </b>
+                    <span>{this.state.book.publisher}</span>
+                </div>
+                <div>
+                    <b>Author: </b>
+                    <span>{this.state.book.author}</span>
+                </div>
+                <div>
+                    <b>Summary: </b>
+                    <span>{this.state.book.summary}</span>
+                </div>
+
             </div>
         );
     }
